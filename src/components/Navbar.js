efd62b14c85bfb3a0a97848ce88ac9719fff6b77
Sibi Chakravarthy P <66554485+SibiChakravarthy7311@users.noreply.github.com>
2021-11-21 15:33:38 +0530

    6 Modules(Deposit, Withdraw, Transfer, Borrow, Payoff, Fundraiser) completed and UI updated

---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React, {Component} from "react";
+import bank from '../dbank.png'
+import './Navbar.css';
+
+class Navbar extends Component {
+    // Main app template to render
+    render() {
+        return (
+            <nav className='navbar navbar-dark fixed-top shadow p-0' style={{backgroundColor: 'black', height: '50px'}}>
+                <a className='navbar-brand col-sm-3 col-md-2 mr-0' 
+                style={{color:'white'}}>
+                    <img src={bank} width='50' height='30' className="d-inline-block align-top" alt="Decentralized Bank"/>&nbsp;&nbsp;
+                    <span>Sibi's Decentralized Bank</span>
+                </a>
+                <ul>
+                    <li className="text-nowrap d-none nav-item d-sm-none d-sm-block" style={{marginTop: '3%'}}>
+                        <small style={{color: 'white'}}>
+                            ACCOUNT NUMBER: {this.props.account} &nbsp;&nbsp;
+                        </small>
+                    </li>
+                </ul>
+            </nav>
+        )
+    }
+}
+
+export default Navbar;
